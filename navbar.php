95cdc5a6a8a7425061dd641a4bc4230d372c7587
leon <leon.haffie-hobday@fujitsu.com>
2023-05-05 10:42:22 +0100

    merge to master

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="css/website.css">
+<nav>
+    <div class="nav-wrapper">
+      <div class="logo"><a href="#">Fallowfield Fashion</a></div>
+      <div class="menu-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul class="nav-menu">
+        <li><a href="#">Locations</a></li>
+        <li class="dropdown">
+          <a href="#">Seasonal</a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Spring</a></li>
+            <li><a href="#">Summer</a></li>
+            <li><a href="#">Autumn</a></li>
+            <li><a href="#">Winter</a></li>
+          </ul>
+        </li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Trends</a></li>
+      </ul>
+    </div>
+  </nav>
